e90b8a59715189a295779acd75658ae0c38c8f52
borgesan <andredesouza.borges@gmail.com>
2023-05-02 21:26:30 -0400

    First commit

---
+++ src/main/java/com/example/punchbag/PunchbagApplication.java
@@ -0,0 +1,13 @@
+package com.example.punchbag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunchbagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunchbagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/punchbag/PunchbagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.punchbag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunchbagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
